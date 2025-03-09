6584b7e8a25d5bf05992f32d76ba171aef8b1da8
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-03-09 12:50:58 +0530

    Display profile

--- api/models/user.modal.js
+++ api/models/user.modal.js
@@ -20,8 +20,8 @@ const userSchema = new mongoose.Schema({
 
     avatar: {
         type: String,
-        default: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-    }
+        default: "https://cdn.vectorstock.com/i/500p/95/56/user-profile-icon-avatar-or-person-vector-45089556.jpg"
+    },
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
